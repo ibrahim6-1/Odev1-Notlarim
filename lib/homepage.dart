20dcd917c292f8b1a36dd1916d2264ddb31dbab0
Ibrahim Balta <baltaibrahim584@gmail.com>
2023-01-11 18:07:27 +0300

    appbar ve bottomNavigationbar eklendi

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -12,7 +12,30 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Lottie.network("https://assets1.lottiefiles.com/packages/lf20_svy4ivvy.json"),),
+      appBar: AppBar(
+        title: const Center(
+          child: Text(
+            "Animasyon ekranı",
+            style: TextStyle(fontSize: 21),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: "Anasayfa",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.map),
+            label: "Harita",
+          ),
+        ],
+      ),
+      body: Center(
+        child: Lottie.network(
+            "https://assets1.lottiefiles.com/packages/lf20_svy4ivvy.json"),
+      ),
     );
   }
 }
